d15710923fb3d6addda1f48cb1b36a5a27021026
Your Name <romdenielle.navarro@email.lcup.edu.ph>
2025-10-18 21:52:51 +0800

    Add padding to navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { memo } from 'react';
 const Navbar = () => {
   return (
 
-      <nav className='bg-main text-dark py-2 px-4 w-full'>
+      <nav className='bg-main text-dark py-4 px-6 w-full'>
         <div className='flex justify-between items-center w-full'>
-            <div className='font-bold text-lg px-4 text-primary'>
+            <div className='font-bold text-lg px-6 text-primary'>
                 Rom Denielle Navarro
             </div>
-            <div className='space-x-8 px-4'>
+            <div className='space-x-8 px-6'>
                 <a href="#about" className='text-primary font-medium hover:text-dark'>About</a>
                 <a href="#projects" className='text-primary font-medium hover:text-dark'>Projects</a>
                 <a href="#contacts" className='text-primary font-medium hover:text-dark'>Contact</a>
